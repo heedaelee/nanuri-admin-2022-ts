161235c4ca03fe4657ca237ae4aa41f3187ef383
heedaelee <adguy72@gmail.com>
2022-09-16 15:12:32 +0900

    fix : kakaoauth부분 실서버 연결로 전환

--- src/pages/KakaoCallBack.tsx
+++ src/pages/KakaoCallBack.tsx
@@ -84,31 +84,31 @@ const KakaoCallBack = () => {
                        * res: posts:[], favorite_posts:[], other user info..
                        * */
                       //FORTEST: Test용
-                      setUserInfo(res.data, ourServerToken);
+                      // setUserInfo(res.data, ourServerToken);
 
                       //NOTE:실제 사용
-                      // axios
-                      //   .get(User.ALL + uuid + "/", {
-                      //     headers: {
-                      //       Authorization: `Token ${ourServerToken}`,
-                      //     },
-                      //   })
-                      //   .then((res) => {
-                      //     if (res.data) {
-                      //       console.log("res.data :");
-                      //       console.log(res.data);
-                      //       if (res.data.is_admin) {
-                      //         setUserInfo(res.data, ourServerToken);
-                      //       } else {
-                      //         console.log("admin false");
-                      //         window.alert(
-                      //           "권한이 없습니다 \n 관리자에게 문의하세요 :("
-                      //         );
-                      //       }
-                      //     } else {
-                      //       console.log("user data 없음!");
-                      //     }
-                      //   });
+                      axios
+                        .get(User.ALL + uuid + "/", {
+                          headers: {
+                            Authorization: `Token ${ourServerToken}`,
+                          },
+                        })
+                        .then((res) => {
+                          if (res.data) {
+                            console.log("res.data :");
+                            console.log(res.data);
+                            if (res.data.is_admin) {
+                              setUserInfo(res.data, ourServerToken);
+                            } else {
+                              console.log("admin false");
+                              window.alert(
+                                "권한이 없습니다 \n 관리자에게 문의하세요 :("
+                              );
+                            }
+                          } else {
+                            console.log("user data 없음!");
+                          }
+                        });
                     } else {
                       if (!res.data.token) {
                         console.log("우리 token 없음!");
